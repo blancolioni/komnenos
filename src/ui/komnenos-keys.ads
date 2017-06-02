c14c9d95d21a73977c511a7a5bc526280a37e53b
Fraser Wilson <blancolioni@gmail.com>
2017-06-02 17:03:44 +0200

    Komnenos.Keys: separated modifiers from key type (so they can also be used with mouse clicks)

--- src/ui/komnenos-keys.ads
+++ src/ui/komnenos-keys.ads
@@ -1,18 +1,32 @@
 package Komnenos.Keys is
 
+   type Modifier_Keys is private;
+
+   function Shift   (Modifier : Modifier_Keys) return Boolean;
+   function Control (Modifier : Modifier_Keys) return Boolean;
+   function Alt     (Modifier : Modifier_Keys) return Boolean;
+   function Meta    (Modifier : Modifier_Keys) return Boolean;
+
+   function Modifier
+     (Shift   : Boolean       := False;
+      Control : Boolean       := False;
+      Alt     : Boolean       := False;
+      Meta    : Boolean       := False)
+      return Modifier_Keys;
+
    type Komnenos_Key is private;
 
-   function Shift   (Key : Komnenos_Key) return Boolean;
-   function Control (Key : Komnenos_Key) return Boolean;
-   function Mod1    (Key : Komnenos_Key) return Boolean;
-   function Mod2    (Key : Komnenos_Key) return Boolean;
+   function Modify (Key      : Komnenos_Key;
+                    Modifier : Modifier_Keys)
+                    return Komnenos_Key;
 
    function Modify (Key     : Komnenos_Key;
                     Shift   : Boolean       := False;
                     Control : Boolean       := False;
-                    Mod1    : Boolean       := False;
-                    Mod2    : Boolean       := False)
-                   return Komnenos_Key;
+                    Alt     : Boolean       := False;
+                    Meta    : Boolean       := False)
+                    return Komnenos_Key
+   is (Modify (Key, Modifier (Shift, Control, Alt, Meta)));
 
    function Is_Character (Key  : Komnenos_Key) return Boolean;
    --  Return True if Key represents a self-inserting character
@@ -20,7 +34,7 @@ package Komnenos.Keys is
    function Is_Function  (Key : Komnenos_Key) return Boolean;
    --  return True if Key represents a function key
 
-   function Character_Key (Ch    : Character) return Komnenos_Key;
+   function Character_Key (Ch : Character) return Komnenos_Key;
    function Get_Character (Key : Komnenos_Key) return Character;
 
    function Raw_Key (Key : Komnenos_Key) return Komnenos_Key;
@@ -42,7 +56,12 @@ package Komnenos.Keys is
 
 private
 
+   type Modifier_Key is (Shift, Control, Alt, Meta);
+
+   type Modifier_Keys is array (Modifier_Key) of Boolean;
+
    type Komnenos_Key is mod 16#10_0000#;
+
    subtype Real_Key is Komnenos_Key range 1 .. 16#FFFF#;
 
    Null_Key    : constant Komnenos_Key := 16#0000#;
@@ -64,9 +83,46 @@ private
    subtype Function_Keys is Komnenos_Key range 16#F100# .. 16#F1FF#;
 
    Key_Mask     : constant Komnenos_Key := 16#FFFF#;
-   Shift_Mask   : constant Komnenos_Key := 16#1_0000#;
-   Control_Mask : constant Komnenos_Key := 16#2_0000#;
-   Mod1_Mask    : constant Komnenos_Key := 16#4_0000#;
-   Mod2_Mask    : constant Komnenos_Key := 16#8_0000#;
+
+   Modifier_Masks : constant array (Modifier_Key) of Komnenos_Key :=
+                      (Shift   => 16#1_0000#,
+                       Control => 16#2_0000#,
+                       Alt     => 16#4_0000#,
+                       Meta    => 16#8_0000#);
+
+   function Modifier
+     (Shift   : Boolean       := False;
+      Control : Boolean       := False;
+      Alt     : Boolean       := False;
+      Meta    : Boolean       := False)
+      return Modifier_Keys
+   is (Shift, Control, Alt, Meta);
+
+   function Shift   (Modifier : Modifier_Keys) return Boolean
+   is (Modifier (Shift));
+
+   function Control (Modifier : Modifier_Keys) return Boolean
+   is (Modifier (Control));
+
+   function Alt     (Modifier : Modifier_Keys) return Boolean
+   is (Modifier (Alt));
+
+   function Meta    (Modifier : Modifier_Keys) return Boolean
+   is (Modifier (Meta));
+
+   function Is_Character (Key  : Komnenos_Key) return Boolean
+   is ((Key and not Modifier_Masks (Shift)) in 32 .. 126);
+
+   function Character_Key (Ch : Character) return Komnenos_Key
+   is (Character'Pos (Ch));
+
+   function Get_Character (Key : Komnenos_Key) return Character
+   is (Character'Val (Key and Key_Mask));
+
+   function Raw_Key (Key : Komnenos_Key) return Komnenos_Key
+   is (Key and Key_Mask);
+
+   function Is_Function  (Key : Komnenos_Key) return Boolean
+   is (Key in Function_Keys);
 
 end Komnenos.Keys;
