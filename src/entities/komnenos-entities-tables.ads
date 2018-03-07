39ed944a3314538a12c7bbf45c05b78ec9cda7e0
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2018-03-07 09:37:28 +0100

    Komnenos: added active table

--- src/entities/komnenos-entities-tables.ads
+++ src/entities/komnenos-entities-tables.ads
@@ -8,4 +8,14 @@ package Komnenos.Entities.Tables is
      (Path  : String;
       Table : not null access Entity_Table_Interface'Class);
 
+   procedure New_Table
+     (Name  : String;
+      Store : not null access Program_Store_Interface'Class);
+
+   function Active_Table
+     return access Entity_Table_Interface'Class;
+
+   procedure Set_Active_Table
+     (Table : not null access Entity_Table_Interface'Class);
+
 end Komnenos.Entities.Tables;
